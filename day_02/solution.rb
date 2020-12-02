f833a606594f7101d9a844763eb964e278ff5bb1
Stuart McHattie <stuart.mchattie@gmail.com>
2020-12-02 20:44:56 +0000

    Update day 2 description and solution for part 2

--- day_02/solution.rb
+++ day_02/solution.rb
@@ -11,4 +11,15 @@ valid_passwords = passwords.select do |p_array|
   p_array[0] <= char_count && char_count <= p_array[1]
 end
 
-puts "Number of valid passwords: #{valid_passwords.count}"
+puts 'Part 1:'
+puts "  Number of valid passwords: #{valid_passwords.count}"
+
+valid_passwords_2 = passwords.select do |p|
+  pattern = "(^#{'.' * (p[0] - 1)}#{p[2]}#{'.' * (p[1] - p[0] - 1)}[^#{p[2]}])|" +
+            "(^#{'.' * (p[0] - 1)}[^#{p[2]}]#{'.' * (p[1] - p[0] - 1)}#{p[2]})"
+  p[3] =~ Regexp.new(pattern)
+end
+
+puts
+puts 'Part 2:'
+puts "  Number of valid passwords: #{valid_passwords_2.count}"

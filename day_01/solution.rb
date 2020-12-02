6dd799948b6b4aaf1234ffcfed7ba90bec389d3c
Stuart McHattie <stuart.mchattie@gmail.com>
2020-12-02 20:02:17 +0000

    Update the description and solution for day 1 part 2

--- day_01/solution.rb
+++ day_01/solution.rb
@@ -10,3 +10,15 @@ input_numbers.each_with_index do |num1, idx|
     end
   end
 end
+
+input_numbers.each_with_index do |num1, idx1|
+  input_numbers.drop(idx1 + 1).each_with_index do |num2, idx2|
+    input_numbers.drop(idx2 + 1).each do |num3|
+      if (num1 + num2 + num3 == 2020) then
+        puts "#{num1} + #{num2} + #{num3} = 2020"
+        puts "#{num1} x #{num2} x #{num3} = #{num1 * num2 * num3}"
+        puts
+      end
+    end
+  end
+end

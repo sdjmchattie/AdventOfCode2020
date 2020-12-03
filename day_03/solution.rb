4ff3817d5885672612a1043ef2679a72c3948e3e
Stuart McHattie <stuart.mchattie@gmail.com>
2020-12-03 09:17:53 +0000

    Update description and solution for day 3 part 2

--- day_03/solution.rb
+++ day_03/solution.rb
@@ -1,21 +1,35 @@
 #!/usr/bin/env ruby
 
-map = File.readlines('input.txt').map { |line| line.strip }
+def count_trees_hit(map, d_x, d_y)
+  map_width = map[0].length
 
-d_x = 3
-d_y = 1
+  trees_hit = 0
+  cur_x = 0
+  cur_y = 0
 
-map_width = map[0].length
+  while cur_y < map.count
+    trees_hit += 1 if map[cur_y][cur_x] == '#'
 
-trees_hit = 0
-cur_x = 0
-cur_y = 0
+    cur_x = (cur_x + d_x) % map_width
+    cur_y += d_y
+  end
 
-while cur_y < map.count
-  trees_hit += 1 if map[cur_y][cur_x] == '#'
-
-  cur_x = (cur_x + d_x) % map_width
-  cur_y += d_y
+  return trees_hit
 end
 
-puts "Encountered trees: #{trees_hit}"
+map = File.readlines('input.txt').map { |line| line.strip }
+
+puts 'Part 1'
+puts "  Encountered trees: #{count_trees_hit(map, 3, 1)}"
+
+hits = [
+  count_trees_hit(map, 1, 1),
+  count_trees_hit(map, 3, 1),
+  count_trees_hit(map, 5, 1),
+  count_trees_hit(map, 7, 1),
+  count_trees_hit(map, 1, 2)
+]
+
+puts
+puts 'Part 2'
+puts "  Product of hit trees: #{hits.reduce(:*)}"

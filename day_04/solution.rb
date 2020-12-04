a94f7e7e109b58e13cfd8e7a4ebab8a3807324b2
Stuart McHattie <stuart.mchattie@gmail.com>
2020-12-04 08:38:49 +0000

    Make height check for units more comprehensive

--- day_04/solution.rb
+++ day_04/solution.rb
@@ -26,11 +26,15 @@ valid_passports_2 = valid_passports_1.select do |pp|
   valid_birth_year = pp['byr'].to_i >= 1920 && pp['byr'].to_i <= 2002
   valid_issue_year = pp['iyr'].to_i >= 2010 && pp['iyr'].to_i <= 2020
   valid_expire_year = pp['eyr'].to_i >= 2020 && pp['eyr'].to_i <= 2030
+
   valid_height = if pp['hgt'].end_with?('cm')
     pp['hgt'].to_i >= 150 && pp['hgt'].to_i <= 193
-  else
+  elsif pp['hgt'].end_with?('in')
     pp['hgt'].to_i >= 59 && pp['hgt'].to_i <= 76
+  else
+    false
   end
+
   valid_hair_colour = pp['hcl'] =~ /^#[0-9A-Fa-f]{6}$/
   valid_eye_colour = %w(amb blu brn gry grn hzl oth).include?(pp['ecl'])
   valid_passport_id = pp['pid'] =~ /^[0-9]{9}$/

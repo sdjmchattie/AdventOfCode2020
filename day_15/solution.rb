70141ba749bfc8403350f7e39996105662fb396b
Stuart McHattie <stuart.mchattie@gmail.com>
2020-12-15 08:31:11 +0000

    Add solution for day 15

--- day_15/solution.rb
+++ day_15/solution.rb
@@ -1,10 +1,34 @@
 #!/usr/bin/env ruby
 
-raw_input = File.readlines('input.txt')
+def game_state_at_turn(initial, target_turn)
+  sequence = initial[0..-1]
+  prev = initial.last
+  turn = initial.count
+
+  last_spoken = Hash.new do |hash, key|
+    hash[key] = sequence.index(key) + 1 if sequence.include?(key)
+  end
+
+  while turn < target_turn
+    if last_spoken[prev].nil?
+      new_entry = 0
+    else
+      new_entry = turn - last_spoken[prev]
+    end
+
+    last_spoken[prev] = turn
+    prev = new_entry
+    turn += 1
+  end
+
+  return(prev)
+end
+
+raw_input = [ 6, 13, 1, 15, 2, 0 ]
 
 puts 'Part 1'
-puts "  Answer goes here."
+puts "  Number at turn 2020 is #{game_state_at_turn(raw_input, 2020)}"
 
 puts
 puts 'Part 2'
-puts "  Answer goes here."
+puts "  Number at turn 30000000 is #{game_state_at_turn(raw_input, 30000000)}"

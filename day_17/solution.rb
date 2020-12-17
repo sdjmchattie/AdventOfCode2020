53f39bc7c38c19844ffcbcd0eb17af9170395dd7
Stuart McHattie <stuart.mchattie@gmail.com>
2020-12-17 23:19:26 +0000

    Add the solution to day 17 part 2
    It's VERY slow so could be optimised

--- day_17/solution.rb
+++ day_17/solution.rb
@@ -2,7 +2,10 @@
 
 class Game
 
-  def initialize(initial_state, max_cycles)
+  attr_reader :dimensions
+
+  def initialize(initial_state, max_cycles, dimensions)
+    self.dimensions = dimensions
     self.states = [ create_state(initial_state, max_cycles) ]
   end
 
@@ -10,51 +13,60 @@ class Game
     Marshal.load(Marshal.dump(states.last))
   end
 
+  def map_state(state, indices)
+    if indices.count == self.dimensions
+      active = active_neighbours(indices)
+      case state
+      when '.'
+        active == 3 ? '#' : '.'
+      when '#'
+        (active == 2 || active == 3) ? '#' : '.'
+      end
+    else
+      state.each_with_index.map do |next_state, idx|
+        map_state(next_state, indices + [idx])
+      end
+    end
+  end
+
   def run_cycle(num = 1)
     num.times do
       puts "Running cycle #{states.count}..."
-      states << self.current_state.each_with_index.map do |slice, z|
-        slice.each_with_index.map do |row, y|
-          row.each_with_index.map do |cube, x|
-            active_neighbours = neighbour_values(self.current_state, z, y, x)
-                .count { |v| v == '#' }
-            case cube
-            when '.'
-              active_neighbours == 3 ? '#' : '.'
-            when '#'
-              (active_neighbours == 2 || active_neighbours == 3) ? '#' : '.'
-            end
-          end
-        end
-      end
+      states << map_state(self.current_state, [])
     end
   end
 
 private
 
   attr_accessor :states
+  attr_writer :dimensions
+
+  def create_dimension(number, length, initial_x, initial_y)
+    return '.' if number == 0
+
+    actual_length = length
+    actual_length += initial_x if number == 1
+    actual_length += initial_y if number == 2
+    actual_length += 1 if number >= 3
+
+    (0...actual_length).map do
+      create_dimension(number - 1, length, initial_x, initial_y)
+    end
+  end
 
   def create_state(state_array, max_cycles)
+    length_x = state_array.first.count
+    length_y = state_array.count
     extra = max_cycles * 2
 
-    # Create empty state large enough for number of cycles
-    state = Array.new
-    (extra + 1).times do
-      slice = Array.new
-      (extra + state_array.count).times do
-        row = Array.new
-        (extra + state_array.first.count).times do
-          row << '.'
-        end
-
-        slice << row
-      end
-
-      state << slice
-    end
+    state = create_dimension(dimensions, extra, length_x, length_y)
 
     # Populate centre of state with initial values
-    state_slice = state[max_cycles]
+    state_slice = state
+    (dimensions - 2).times do
+      state_slice = state_slice[max_cycles]
+    end
+
     state_array.count.times do |y|
       state_y = max_cycles + y
       state_array.first.count.times do |x|
@@ -66,27 +78,20 @@ private
     state
   end
 
-  def neighbour_values(state, z, y, x)
-    values = Array.new
-
-    (-1..1).each do |dz|
-      nz = z + dz
-      next if nz < 0 || nz >= state.count
-      layer = state[nz]
-      (-1..1).each do |dy|
-        ny = y + dy
-        next if ny < 0 || ny >= layer.count
-        row = layer[ny]
-        (-1..1).each do |dx|
-          next if dx == 0 && dy == 0 && dz == 0
-          nx = x + dx
-          next if nx < 0 || nx >= row.count
-          values << row[nx]
-        end
-      end
+  def active_count(state, indices, deltas)
+    if indices.count == 0
+      return state == '#' && !deltas.all? { |d| d == 0 } ? 1 : 0
     end
 
-    values
+    (-1..1).sum do |d|
+      i = indices[0] + d
+      next 0 if i < 0 || i >= state.count
+      active_count(state[i], indices.drop(1), deltas + [d])
+    end
+  end
+
+  def active_neighbours(indices)
+    active_count(self.current_state, indices, Array.new)
   end
 
 end
@@ -96,7 +101,7 @@ clean_input = raw_input.map(&:strip).map(&:chars)
 
 puts 'Part 1'
 
-game = Game.new(clean_input, 6)
+game = Game.new(clean_input, 6, 3)
 game.run_cycle(6)
 
 active_count = game.current_state.reduce(0) do |tot_z, layer|
@@ -110,4 +115,15 @@ puts "  Active cubes after 6 cycles = #{active_count}"
 
 puts
 puts 'Part 2'
+
+game = Game.new(clean_input, 6, 4)
+game.run_cycle(6)
+
+active_count = game.current_state.reduce(0) do |tot_z, layer|
+  tot_z + layer.reduce(0) do |tot_y, row|
+    tot_y + row.count { |cube| cube == '#' }
+  end
+end
+
+puts
 puts "  Answer goes here."

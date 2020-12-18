baebe3b99fbf30ef27afdaada581737179940779
Stuart McHattie <stuart.mchattie@tessella.com>
2020-12-18 22:00:27 +0000

    Add solution to day 18 part 1

--- day_18/solution.rb
+++ day_18/solution.rb
@@ -1,9 +1,77 @@
 #!/usr/bin/env ruby
 
+def find_closing_parenthesis(expression)
+  count = 0
+  idx = 0
+  loop do
+    case expression[idx]
+    when '('
+      count += 1
+    when ')'
+      count -= 1
+    end
+
+    return idx if count == 0
+    idx += 1
+    return nil if idx == expression.length
+  end
+end
+
+def split_next(expression)
+  stripped = expression.strip
+  idx = 0
+  if stripped[0] == '('
+    idx = find_closing_parenthesis(stripped)
+  else
+    idx = stripped.index(' ')
+  end
+
+  [stripped[0..idx], stripped[idx+1..-1]].map(&:strip)
+end
+
+def split_expression(expression)
+  parts = Array.new
+  remaining_expression = expression.strip
+  while remaining_expression.include? ' '
+    split = split_next(remaining_expression)
+    parts << split[0]
+    remaining_expression = split[1]
+  end
+
+  parts << remaining_expression
+  parts.reject { |p| p.length == 0 }
+end
+
+def evaluate(expression)
+  split = split_expression(expression)
+
+  while split.count > 1
+    left = split.shift
+    operator = split.shift
+    right = split.shift
+
+    left = evaluate(left[1..-2]) if left[0] == '('
+    right = evaluate(right[1..-2]) if right[0] == '('
+
+    case operator
+    when '+'
+      split.unshift (left.to_i + right.to_i).to_s
+    when '*'
+      split.unshift (left.to_i * right.to_i).to_s
+    else
+      abort("Unexpected operator found: #{operator}")
+    end
+  end
+
+  split[0].to_i
+end
+
 raw_input = File.readlines('input.txt')
 
+exp_sum = raw_input.sum { |exp| evaluate(exp) }
+
 puts 'Part 1'
-puts "  Answer goes here."
+puts "  Sum of all evaluated lines is #{exp_sum}"
 
 puts
 puts 'Part 2'

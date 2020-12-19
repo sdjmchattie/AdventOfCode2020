9be3f1cd69da24f00ef619cb27f42851205a1ef4
Stuart McHattie <stuart.mchattie@gmail.com>
2020-12-19 09:18:37 +0000

    Start day 19 solution

--- day_19/solution.rb
+++ day_19/solution.rb
@@ -1,6 +1,12 @@
 #!/usr/bin/env ruby
 
 raw_input = File.readlines('input.txt')
+sections = raw_input.map(&:strip).slice_before('').to_a
+rules = sections[0]
+messages = sections[1].drop(1)
+
+puts rules.count
+puts messages.count
 
 puts 'Part 1'
 puts "  Answer goes here."

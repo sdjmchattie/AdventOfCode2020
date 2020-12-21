bf4d462cd6d6e8badbe036dbaa70416b5d1378ff
Stuart McHattie <stuart.mchattie@tessella.com>
2020-12-21 20:09:14 +0000

    Update day 19 part 1 solution

--- day_19/solution.rb
+++ day_19/solution.rb
@@ -1,5 +1,56 @@
 #!/usr/bin/env ruby
 
+class Resolver
+  attr_reader :rules, :resolved_rules
+
+  def initialize(rules)
+    @rules = rules.dup
+  end
+
+  def resolve
+    self.resolved_rules = Hash.new
+    remaining_rules = self.rules.dup
+    count = 0
+
+    while remaining_rules.count > 0 do
+      newly_resolved_rules = remaining_rules.select do |key, _|
+        remaining_rules[key].flatten == remaining_rules[key]
+      end
+
+      self.resolved_rules = self.resolved_rules.merge(newly_resolved_rules)
+      remaining_rules = remaining_rules.delete_if do |key|
+        newly_resolved_rules.keys.include? key
+      end
+
+      newly_resolved_rules.each do |resolved_num, resolved_msgs|
+        remaining_rules = remaining_rules.map do |remain_num, remain_msgs|
+          updated_msgs = remain_msgs.flat_map do |msg|
+            next [ msg ] unless msg.is_a?(Array)
+
+            product_array = msg.map do |m|
+              next resolved_msgs if m == resolved_num
+              [ m ]
+            end
+
+            new_msgs = product_array[0].product(*product_array[1..-1])
+            new_msgs.map do |p_msg|
+              next p_msg.join if p_msg.all? { |msg_part| msg_part.is_a? String }
+              p_msg
+            end
+          end
+
+          [ remain_num, updated_msgs ]
+        end.to_h
+      end
+    end
+  end
+
+private
+
+  attr_writer :resolved_rules
+
+end
+
 raw_input = File.readlines('input.txt')
 sections = raw_input.map(&:strip).slice_before('').to_a
 rules = sections[0]
@@ -22,46 +73,13 @@ rule_map = rules.map do |rule_text|
   next [m[1].to_i, [[m[2].to_i]]] unless m.nil?
 end.to_h
 
-resolved_rules = Hash.new
-remaining_rules = rule_map
-count = 0
-
-while remaining_rules.count > 0 do
-  newly_resolved_rules = remaining_rules.select do |key, _|
-    remaining_rules[key].flatten == remaining_rules[key]
-  end
-
-  resolved_rules = resolved_rules.merge(newly_resolved_rules)
-  remaining_rules = remaining_rules.delete_if do |key|
-    newly_resolved_rules.keys.include? key
-  end
-
-  newly_resolved_rules.each do |resolved_num, resolved_msgs|
-    remaining_rules = remaining_rules.map do |remain_num, remain_msgs|
-      updated_msgs = remain_msgs.flat_map do |msg|
-        next [ msg ] unless msg.is_a?(Array)
-
-        product_array = msg.map do |m|
-          next resolved_msgs if m == resolved_num
-          [ m ]
-        end
-
-        new_msgs = product_array[0].product(*product_array[1..-1])
-        new_msgs.map do |p_msg|
-          next p_msg.join if p_msg.all? { |msg_part| msg_part.is_a? String }
-          p_msg
-        end
-      end
-
-      [ remain_num, updated_msgs ]
-    end.to_h
-  end
-end
-
-ok_messages = resolved_rules[0]
+resolver = Resolver.new(rule_map)
+resolver.resolve
+ok_messages = resolver.resolved_rules[0]
+valid_message_count = messages.count { |msg| ok_messages.include? msg }
 
 puts 'Part 1'
-puts "  Number of valid messages is #{messages.count { |msg| ok_messages.include? msg }}"
+puts "  Number of valid messages is #{valid_message_count}"
 
 puts
 puts 'Part 2'

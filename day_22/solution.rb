c0a92ac679a92643a726a6d1ac813d2b530319b0
Stuart McHattie <stuart.mchattie@tessella.com>
2020-12-22 22:13:15 +0000

    Add day 22 part 1 solution

--- day_22/solution.rb
+++ day_22/solution.rb
@@ -1,9 +1,26 @@
 #!/usr/bin/env ruby
 
 raw_input = File.readlines('input.txt')
+inputs = raw_input.map(&:strip).slice_before('').to_a
+deck_1 = inputs[0].drop(1).map(&:to_i)
+deck_2 = inputs[1].drop(2).map(&:to_i)
+
+while deck_1.count != 0 && deck_2.count != 0
+  card_1 = deck_1.shift
+  card_2 = deck_2.shift
+  if card_1 > card_2
+    deck_1 += [card_1, card_2]
+  else
+    deck_2 += [card_2, card_1]
+  end
+end
+
+winner = deck_1.count == 0 ? deck_2 : deck_1
+
+winner = winner.reverse.each_with_index.map { |card, idx| card * (idx + 1) }
 
 puts 'Part 1'
-puts "  Answer goes here."
+puts "  Winner's score is #{winner.sum}"
 
 puts
 puts 'Part 2'

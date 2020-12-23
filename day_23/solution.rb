bd3cb199b90448462ed784ad44d419bbf67e4e5d
Stuart McHattie <stuart.mchattie@tessella.com>
2020-12-23 09:55:39 +0000

    Implement a faster game for day 23 part 2

--- day_23/solution.rb
+++ day_23/solution.rb
@@ -1,71 +1,71 @@
 #!/usr/bin/env ruby
 
-class Game
+class FastGame
   attr_reader :final_layout
 
-  def initialize(starting_position)
-    self.initial = starting_position
+  def initialize(initial)
+    self.circle = Hash.new
+    (initial.count - 1).times do |i|
+      self.circle[initial[i]] = initial[i + 1]
+    end
+
+    self.current = initial[0]
+    self.circle[initial[-1]] = self.current
   end
 
   def play(moves)
-    cups = self.initial.dup
-    lowest = cups.min
-    highest = cups.max
-    current = 0
+    lowest = self.circle.keys.min
+    highest = self.circle.keys.max
 
     moves.times do |move|
-      puts "Starting move #{move}..." if move % 1000 == 0
-      moving = cups[(current + 1)..(current + 3)]
-      3.times do
-        cups.delete_at(current + 1)
-      end
+      puts "Starting move #{move}..." if move % 1000000 == 0
 
-      destination = cups[current]
+      moving_1 = self.circle[self.current]
+      moving_2 = self.circle[moving_1]
+      moving_3 = self.circle[moving_2]
+
+      destination = self.current
       loop do
         destination = destination - 1
         destination = highest if destination < lowest
-        break unless moving.include? destination
+        break unless destination == moving_1 || destination == moving_2 || destination == moving_3
       end
 
-      dest_idx = cups.index destination
-      cups.insert(dest_idx + 1, *moving)
-      current += 3 if dest_idx < current
+      self.circle[self.current] = self.circle[moving_3]
+      temp = self.circle[destination]
+      self.circle[destination] = moving_1
+      self.circle[moving_3] = temp
 
-      current += 1
-      if current > cups.count - 4
-        cups = cups[current..-1] + cups[0...current]
-        current = 0
-      end
+      self.current = self.circle[self.current]
     end
 
-    if cups.first == 1
-      self.final_layout = cups.drop(1)
-    else
-      sliced_cups = cups.slice_before(1).to_a
-      self.final_layout = (sliced_cups[1].drop(1) + sliced_cups[0])
+    cur = 1
+    self.final_layout = Array.new
+    loop do
+      cur = self.circle[cur]
+      break if cur == 1
+      self.final_layout << cur
     end
   end
 
 private
 
-  attr_accessor :initial
+  attr_accessor :circle, :current
   attr_writer :final_layout
 
 end
 
+
 puts 'Part 1'
 initial = '156794823'.chars.map(&:to_i)
-game = Game.new(initial)
+game = FastGame.new(initial)
 game.play(100)
 puts "  Final layout is #{game.final_layout.join}"
 
 puts
 puts 'Part 2'
 second_initial = initial + (10..1000000).to_a
-game = Game.new(second_initial)
+game = FastGame.new(second_initial)
 game.play(10000000)
-
-File.open("long_game.dat", "w") { |to_file| Marshal.dump(game, to_file) }
-
 two_cups = game.final_layout[0..1]
 puts "  Product of two cups is #{two_cups.reduce(&:*)}"

3ce508eeefb1ea5db29e9d731b81e87226a52ac9
Stuart McHattie <stuart.mchattie@tessella.com>
2020-12-23 09:26:55 +0000

    Add very slow solution for day 23 part 2

--- day_23/solution.rb
+++ day_23/solution.rb
@@ -7,34 +7,42 @@ class Game
     self.initial = starting_position
   end
 
-  def play
-    cups = self.initial.chars.map(&:to_i)
+  def play(moves)
+    cups = self.initial.dup
     lowest = cups.min
     highest = cups.max
+    current = 0
 
-    100.times do
-      current = cups[0]
-      moving = cups[1..3]
-      cups = cups[4..-1]
+    moves.times do |move|
+      puts "Starting move #{move}..." if move % 1000 == 0
+      moving = cups[(current + 1)..(current + 3)]
+      3.times do
+        cups.delete_at(current + 1)
+      end
 
-      destination = current
+      destination = cups[current]
       loop do
         destination = destination - 1
         destination = highest if destination < lowest
-        break if cups.include? destination
+        break unless moving.include? destination
       end
 
-      sliced_cups = cups.slice_after(destination).to_a
-      cups = sliced_cups[0] + moving
-      cups += sliced_cups[1] if sliced_cups.count > 1
-      cups << current
+      dest_idx = cups.index destination
+      cups.insert(dest_idx + 1, *moving)
+      current += 3 if dest_idx < current
+
+      current += 1
+      if current > cups.count - 4
+        cups = cups[current..-1] + cups[0...current]
+        current = 0
+      end
     end
 
     if cups.first == 1
-      self.final_layout = cups.drop(1).join
+      self.final_layout = cups.drop(1)
     else
       sliced_cups = cups.slice_before(1).to_a
-      self.final_layout = (sliced_cups[1].drop(1) + sliced_cups[0]).join
+      self.final_layout = (sliced_cups[1].drop(1) + sliced_cups[0])
     end
   end
 
@@ -45,12 +53,19 @@ private
 
 end
 
-game = Game.new('156794823')
-game.play
-
 puts 'Part 1'
-puts "  Final layout is #{game.final_layout}"
+initial = '156794823'.chars.map(&:to_i)
+game = Game.new(initial)
+game.play(100)
+puts "  Final layout is #{game.final_layout.join}"
 
 puts
 puts 'Part 2'
-puts "  Answer goes here."
+second_initial = initial + (10..1000000).to_a
+game = Game.new(second_initial)
+game.play(10000000)
+
+File.open("long_game.dat", "w") { |to_file| Marshal.dump(game, to_file) }
+
+two_cups = game.final_layout[0..1]
+puts "  Product of two cups is #{two_cups.reduce(&:*)}"

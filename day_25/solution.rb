2cc4897692c3969c9ded7e4ed0a8fbab83ebd049
Stuart McHattie <stuart.mchattie@gmail.com>
2020-12-25 08:05:57 +0000

    Add solution to day 25

--- day_25/solution.rb
+++ day_25/solution.rb
@@ -1,10 +1,33 @@
 #!/usr/bin/env ruby
 
-raw_input = File.readlines('input.txt')
+door_key, card_key = [ 19241437, 17346587 ]
+
+subject = 7
+door_loops = 0
+door_value = 1
+loop do
+  door_loops += 1
+  door_value = door_value * subject % 20201227
+  break if door_value == door_key
+end
+
+card_loops = 0
+card_value = 1
+loop do
+  card_loops += 1
+  card_value = card_value * subject % 20201227
+  break if card_value == card_key
+end
+
+encrypt_subject = card_key
+encrypt_value = 1
+door_loops.times do
+  encrypt_value = encrypt_value * encrypt_subject % 20201227
+end
 
 puts 'Part 1'
-puts "  Answer goes here."
+puts "  Encryption key is #{encrypt_value}"
 
 puts
 puts 'Part 2'
-puts "  Answer goes here."
+puts "  There is no part 2!"
